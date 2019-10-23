bfcd3439a86264505d373fbe6db1fc225f5420a8
paul-hernandez99 <paul.hernandez@opendeusto.es>
2019-10-23 20:24:31 +0200

    Registration advanced

--- DeustoPress/src/ventanas/VentanaPrincipal.java
+++ DeustoPress/src/ventanas/VentanaPrincipal.java
@@ -6,6 +6,7 @@ import SQLite.BDManager;
 import exceptions.Exceptions;
 import usuarios.Administrador;
 import usuarios.Usuario;
+import usuarios.UsuarioNormal;
 import ventanas.panels.PanelAdmin;
 import ventanas.panels.PanelUser;
 import javax.swing.JLabel;
@@ -13,9 +14,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
-import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
@@ -23,51 +22,36 @@ import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-import java.awt.event.MouseAdapter;
 import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.KeyboardFocusManager;
-import java.awt.TextField;
-import java.awt.event.MouseMotionAdapter;
 import java.awt.event.MouseMotionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-
-import javax.swing.SwingConstants;
 import javax.swing.border.Border;
-import javax.swing.plaf.basic.BasicToggleButtonUI;
-
-//Mirar diferentes layouts (La de tres franjas puede estar bien), crear un panel con un color neutro para el login
-//y para el registro (cuando se le de al boton que aparezcan las cosas del registro).
-
-
+//Comprobar registro user
 public class VentanaPrincipal extends JFrame 
 {
 	private ArrayList<Usuario> usuarios;
 	private Usuario usuario;
 	
-	private JPanel panelLogin;
-	private JPanel panel1;
+	private JPanel panel;
+	
 	private JLabel logo;
 	private JLabel intro;
+	private JLabel registration;
 	
 	private JTextField username;
-	private JTextField password;
+	private JPasswordField password;
+	private JTextField name;
+	private JTextField email;
+	private JTextField day;
+	private JTextField month;
+	private JTextField year;
 	
 	private JButton btnSignIn;
 	private JButton btnSignUp;
 	private JButton btnCancel;
-	
-	private TextField dia;
-	private TextField mes;
-	private TextField year;
 	
 	public static void main(String[] args) 
 	{
@@ -94,11 +78,11 @@ public class VentanaPrincipal extends JFrame
 		usuarios = bdManager.loadUsers();
 		this.getContentPane().setBackground(Color.WHITE);
 		this.setBounds(670, 60, 600, 900);
-		
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		
 		this.createPanel();
 		this.createIcon();
-		this.createIntro();
+		this.createJlabels();
 		this.createJtextfields();
 		this.createButtons();
 		this.aux();
@@ -107,27 +91,30 @@ public class VentanaPrincipal extends JFrame
 	
 	private void createPanel()
 	{
-		panel1 = new JPanel();
-		panel1.setBackground(Color.WHITE);
+		panel = new JPanel();
+		panel.setBackground(Color.WHITE);
 		Border border = BorderFactory.createLineBorder(new Color(153, 240, 153),3);
-		panel1.setBorder(border);
-		panel1.setLayout(null);
-		this.add(panel1);
+		panel.setBorder(border);
+		panel.setLayout(null);
+		this.add(panel);
 	}
 	
 	private void createIcon()
 	{
 		logo = new JLabel(new ImageIcon("Imagenes/System/Wallpaper.png"));
 		this.add(logo);
-		logo.requestFocusInWindow();
 		
 	}
 	
-	private void createIntro()
+	private void createJlabels()
 	{
 		intro = new JLabel("Welcome to Beermeet:");
 		intro.setFont(new Font("Gill Sans MT", Font.BOLD, 20));
 		this.add(intro);
+		
+		registration = new JLabel("Registration info:");
+		registration.setFont(new Font("Gill Sans MT", Font.BOLD, 20));
+		panel.add(registration);
 	}
 	
 	private void createJtextfields()
@@ -136,33 +123,26 @@ public class VentanaPrincipal extends JFrame
 		username.setFont(new Font("Tahoma", Font.ITALIC, 16));
 		username.setFocusable(false);
 		username.setText("Username");
-		panel1.add(username);
+		panel.add(username);
 		
-		username.addMouseMotionListener(new MouseMotionListener() {
+		username.addMouseMotionListener(new MouseMotionListener() 
+		{
 			
 			@Override
-			public void mouseMoved(MouseEvent e) {
-				// TODO Auto-generated method stub
+			public void mouseMoved(MouseEvent e) 
+			{
 				username.setFocusable(true);
 			}
 
 			@Override
-			public void mouseDragged(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
+			public void mouseDragged(MouseEvent e) {}
 			
 		});
 		
 		username.addFocusListener(new FocusListener() 
 		{
 			@Override
-			public void focusLost(FocusEvent e) 
-			{
-				
-				
-			}
+			public void focusLost(FocusEvent e) {}
 			
 			@Override
 			public void focusGained(FocusEvent e) 
@@ -171,42 +151,128 @@ public class VentanaPrincipal extends JFrame
 			}
 		});
 		
-		
-		password = new JTextField();
+		password = new JPasswordField();
 		password.setFont(new Font("Tahoma", Font.ITALIC, 16));
 		password.setFocusable(false);
+		password.setEchoChar((char)0);
 		password.setText("Password");
-		panel1.add(password);
+		panel.add(password);
 		
-		password.addMouseMotionListener(new MouseMotionListener() {
-			
+		password.addMouseMotionListener(new MouseMotionListener() 
+		{
 			@Override
-			public void mouseMoved(MouseEvent e) {
-				// TODO Auto-generated method stub
+			public void mouseMoved(MouseEvent e) 
+			{
 				password.setFocusable(true);
 			}
 			
 			@Override
-			public void mouseDragged(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
+			public void mouseDragged(MouseEvent e) {}
+		});
+		
+		password.addFocusListener(new FocusListener()
+		{
+			@Override
+			public void focusLost(FocusEvent arg0) {}
+			
+			@Override
+			public void focusGained(FocusEvent arg0) 
+			{
+				password.setText("");
+				password.setEchoChar('*');
 			}
 		});
 		
-		password.addFocusListener(new FocusListener() {
+		name = new JTextField();
+		name.setFont(new Font("Tahoma", Font.ITALIC, 16));
+		name.setVisible(false);
+		name.setText("name");
+		panel.add(name);
+		
+		name.addFocusListener(new FocusListener() 
+		{
+			@Override
+			public void focusLost(FocusEvent e) {}
 			
 			@Override
-			public void focusLost(FocusEvent arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void focusGained(FocusEvent arg0) {
-				// TODO Auto-generated method stub
-				password.setText("");
+			public void focusGained(FocusEvent e) 
+			{
+				name.setText("");
 			}
 		});
+		
+		email = new JTextField();
+		email.setFont(new Font("Tahoma", Font.ITALIC, 16));
+		email.setVisible(false);
+		email.setText("email");
+		panel.add(email);
+		
+		email.addFocusListener(new FocusListener() 
+		{
+			@Override
+			public void focusLost(FocusEvent e) {}
+			
+			@Override
+			public void focusGained(FocusEvent e) 
+			{
+				email.setText("");
+			}
+		});
+		
+		day = new JTextField();
+		day.setFont(new Font("Tahoma", Font.ITALIC, 16));
+		day.setVisible(false);
+		day.setText("day");
+		panel.add(day);
+		
+		day.addFocusListener(new FocusListener() 
+		{
+			@Override
+			public void focusLost(FocusEvent e) {}
+			
+			@Override
+			public void focusGained(FocusEvent e) 
+			{
+				day.setText("");
+			}
+		});
+		
+		month = new JTextField();
+		month.setFont(new Font("Tahoma", Font.ITALIC, 16));
+		month.setVisible(false);
+		month.setText("month");
+		panel.add(month);
+		
+		month.addFocusListener(new FocusListener() 
+		{
+			@Override
+			public void focusLost(FocusEvent e) {}
+			
+			@Override
+			public void focusGained(FocusEvent e) 
+			{
+				month.setText("");
+			}
+		});
+		
+		year = new JTextField();
+		year.setFont(new Font("Tahoma", Font.ITALIC, 16));
+		year.setVisible(false);
+		year.setText("year");
+		panel.add(year);
+		
+		year.addFocusListener(new FocusListener() 
+		{
+			@Override
+			public void focusLost(FocusEvent e) {}
+			
+			@Override
+			public void focusGained(FocusEvent e) 
+			{
+				year.setText("");
+			}
+		});
+		
 	}
 	
 	private void createButtons()
@@ -215,7 +281,7 @@ public class VentanaPrincipal extends JFrame
 		btnSignIn.setFont(new Font("Gill Sans MT", Font.BOLD, 16));
 		btnSignIn.setBackground(new Color(255, 102, 102));
 		btnSignIn.setForeground(new Color(255, 255, 255));
-		panel1.add(btnSignIn);
+		panel.add(btnSignIn);
 		
 		btnSignIn.addActionListener(new ActionListener() 
 		{
@@ -263,15 +329,23 @@ public class VentanaPrincipal extends JFrame
 		btnSignUp.setForeground(new Color(255, 255, 255));
 		btnSignUp.setFont(new Font("Gill Sans MT", Font.BOLD, 16));
 		btnSignUp.setBackground(new Color(255, 102, 102));
-		panel1.add(btnSignUp);
+		panel.add(btnSignUp);
 		
 		btnSignUp.addActionListener(new ActionListener() 
 		{
-			
 			@Override
 			public void actionPerformed(ActionEvent e) 
 			{
+				registrationJButtonsVisible();
 				
+				btnSignUp.addActionListener(new ActionListener() 
+				{
+					@Override
+					public void actionPerformed(ActionEvent arg0) 
+					{
+						createUser();
+					}
+				});
 			}
 		});
 		
@@ -279,7 +353,7 @@ public class VentanaPrincipal extends JFrame
 		btnCancel.setForeground(new Color(255, 255, 255));
 		btnCancel.setFont(new Font("Gill Sans MT", Font.BOLD, 16));
 		btnCancel.setBackground(new Color(153, 240, 153));
-		panel1.add(btnCancel);
+		panel.add(btnCancel);
 		
 		btnCancel.addActionListener(new ActionListener() 
 		{
@@ -293,33 +367,45 @@ public class VentanaPrincipal extends JFrame
 	
 	private void aux()
 	{
-		dia = new TextField();
-		dia.setFont(new Font("Tahoma", Font.ITALIC, 16));
-		dia.setText("day");
-		
-		mes = new TextField();
-		mes.setFont(new Font("Tahoma", Font.ITALIC, 16));
-		mes.setText("month");
-		
-		year = new TextField();
-		year.setFont(new Font("Tahoma", Font.ITALIC, 16));
-		year.setText("year");
-		
 		this.addComponentListener(new ComponentAdapter() 
 		{
 			public void componentResized(ComponentEvent evt)
 			{
-				panel1.setBounds(((getBounds().width)/2)-175, 445, 350, 360);
+				panel.setBounds(((getBounds().width)/2)-175, 445, 350, 360);
 				logo.setBounds(((getBounds().width)/2)-185, 0, 370, 370);
 				intro.setBounds(((getBounds().width)/2)-115, 390, 230, 30);
-				username.setBounds(((panel1.getBounds().width)/2)-110, 40, 220, 30);
-				password.setBounds(((panel1.getBounds().width)/2)-110, 100, 220, 30);
-				btnSignIn.setBounds(((panel1.getBounds().width)/2)-50, 180, 100, 40);
-				btnSignUp.setBounds(((panel1.getBounds().width)/2)-50, 240, 100, 40);
-				btnCancel.setBounds(((panel1.getBounds().width)/2)-50, 300, 100, 40);
+				username.setBounds(((panel.getBounds().width)/2)-110, 30, 220, 30);
+				password.setBounds(((panel.getBounds().width)/2)-110, 80, 220, 30);
+				btnSignIn.setBounds(((panel.getBounds().width)/2)-50, 180, 100, 40);
+				btnSignUp.setBounds(((panel.getBounds().width)/2)-50, 240, 100, 40);
+				btnCancel.setBounds(((panel.getBounds().width)/2)-50, 300, 100, 40);
 			}
 		});
 		
+	}
+	
+	private void registrationJButtonsVisible()
+	{
+		registration.setVisible(true);
+		name.setVisible(true);
+		email.setVisible(true);
+		day.setVisible(true);
+		month.setVisible(true);
+		year.setVisible(true);
+	}
+	
+	private void createUser()
+	{
+		String username = this.username.getText();
+		String password = this.password.getText();
+		String name = this.name.getText();
+		String email = this.email.getText();
+		String fecNac = this.day.getText() +"-"+ this.month.getText() +"-"+ this.year.getText();
+		
+		UsuarioNormal usuario = new UsuarioNormal(username, password, name, email, fecNac);
+		
+		BDManager bdManager = new BDManager();
+		bdManager.saveUser(usuario);
 	}
 	
 	private void comprobarUsuario(String usuario, String password) throws Exceptions
@@ -351,17 +437,6 @@ public class VentanaPrincipal extends JFrame
 		}
 	}
 
-
-	public JPanel getPanelLogin() 
-	{
-		return panelLogin;
-	}
-
-	public void setPanelLogin(JPanel panelLogin) 
-	{
-		this.panelLogin = panelLogin;
-	}
-
 	public ArrayList<Usuario> getUsuarios() 
 	{
 		return usuarios;
@@ -384,32 +459,12 @@ public class VentanaPrincipal extends JFrame
 
 	public JPanel getPanel1() 
 	{
-		return panel1;
+		return panel;
 	}
 
 	public void setPanel1(JPanel panel1) 
 	{
-		this.panel1 = panel1;
-	}
-
-	public JLabel getLogo() 
-	{
-		return logo;
-	}
-
-	public void setLogo(JLabel logo) 
-	{
-		this.logo = logo;
-	}
-
-	public JLabel getIntro() 
-	{
-		return intro;
-	}
-
-	public void setIntro(JLabel intro) 
-	{
-		this.intro = intro;
+		this.panel = panel1;
 	}
 
 	public JTextField getUsername() 
@@ -427,71 +482,9 @@ public class VentanaPrincipal extends JFrame
 		return password;
 	}
 
-	public void setPassword(JTextField password) 
+	public void setPassword(JPasswordField password) 
 	{
 		this.password = password;
 	}
-
-	public JButton getBtnSignIn() 
-	{
-		return btnSignIn;
-	}
-
-	public void setBtnSignIn(JButton btnSignIn) 
-	{
-		this.btnSignIn = btnSignIn;
-	}
-
-	public JButton getBtnSignUp() 
-	{
-		return btnSignUp;
-	}
-
-	public void setBtnSignUp(JButton btnSignUp) 
-	{
-		this.btnSignUp = btnSignUp;
-	}
-
-	public JButton getBtnCancel() 
-	{
-		return btnCancel;
-	}
-
-	public void setBtnCancel(JButton btnCancel) 
-	{
-		this.btnCancel = btnCancel;
-	}
-
-	public TextField getDia() 
-	{
-		return dia;
-	}
-
-	public void setDia(TextField dia) 
-	{
-		this.dia = dia;
-	}
-
-	public TextField getMes() 
-	{
-		return mes;
-	}
-
-	public void setMes(TextField mes) 
-	{
-		this.mes = mes;
-	}
-
-	public TextField getYear() 
-	{
-		return year;
-	}
-
-	public void setYear(TextField year) 
-	{
-		this.year = year;
-	}
-	
-	
 	
 }
